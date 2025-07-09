78074d57dea8ac266062ce6b10251881b5139595
md haseeb <mdhaseeb079@.com>
2025-07-09 09:37:10 +0600

    auth.ts modified

--- auth.ts
+++ auth.ts
@@ -15,14 +15,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   callbacks: {
+    // এই callback-টি সেশনে ব্যবহারকারীর আইডি যোগ করার জন্য
+    // এটি নিশ্চিত করে যে await auth() থেকে পাওয়া session অবজেক্টে id থাকবে
     async session({ session, token }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
       return session;
     },
+
+    // এই callback-টি নিশ্চিত করে যে JWT টোকেনে ডাটাবেসের User ID থাকবে
+    // এটি শুধুমাত্র প্রথমবার সাইন-ইন করার সময় চলে
     async jwt({ token, user }) {
-      if (user) {
+      if (user?.id) {
         token.sub = user.id;
       }
       return token;
